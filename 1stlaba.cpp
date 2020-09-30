c1c3c2ec450b81dc422fcc8859c24f002181ec65
FOOZBY <leha.sharonin2001@mail.ru>
2020-09-30 18:15:29 +0300

    Добавьте файлы проекта.

---
+++ 1stlaba.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>                               
+#include <cmath>                                  
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    double param;
+    cout << "Введите степень: ";
+    cin >> param;
+    cout << "Экспонента числа " << param << " = " << exp(param) << endl; 
+    system("pause");
+    return 0;
+}
